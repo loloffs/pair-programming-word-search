1d45e116eba5552dee721a9af19eaf5adf432ed8
loloffs <lukeoloffs@gmail.com>
2021-01-14 00:03:39 +0000

    pair programming word search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,3 +1,5 @@
+// Pair Programming with Jerrica Joe, Alex Hauka, and Luke Oloffs
+
 const chai = require('chai');
 const assert = chai.assert;
 
@@ -20,7 +22,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +37,26 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'M', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'C', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'H', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'I', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'K', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'E', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'N', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CHICKEN')
+
+    assert.isTrue(result);
+  });
+
+  it("should return undefine if the array given is empty", function() {
+    const result = wordSearch([], "CHICKEN");
+
+    assert.isUndefined(result);
+  })
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,54 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Pair Programming with Jerrica Joe, Alex Hauka, and Luke Oloffs
 
-module.exports = wordSearch
+// Transpose Function from previous PP
+const transpose = function(matrix) {
+    if (matrix === []) {
+      return undefined;
+    }
+    let newArray = [];
+    // for each column (values in nested array at [0]), add an array to newArray
+    for (let x = 0; x < matrix[0].length; x++) {
+      newArray.push([]);
+      // add each element by column into each new array (all 1st elements to array at[0], 2nd elements to array at [1], ...)
+      for (let y = 0; y < matrix.length; y++) {
+        newArray[x].push(matrix[y][x]);
+      }
+    }
+    return newArray;
+  };
+  
+  const searchArrays = (arr, word) => {
+    for (const l of arr) {
+      if (l.includes(word)) {
+        return true;
+      } 
+    };
+    return false;
+  };
+  
+  const wordSearch = (letters, word) => {
+    if (letters.length > 0) {
+      // console.log(letters);
+      // if (letters === []) {
+      //   return undefined;
+      // }
+      // Convert nested arrays to strings 
+      const horizontalJoin = letters.map(l => l.join(''));
+      const verticalJoin = transpose(letters).map(l => l.join(''));
+      // Check the arrays/strings for the word 
+      const horizontal = searchArrays(horizontalJoin, word);
+      const vertical = searchArrays(verticalJoin, word);
+      // Return whether the word is present in either direction
+      if (horizontal || vertical === true) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    else {
+      return undefined;
+    }
+};
+    
+module.exports = wordSearch;
+   
